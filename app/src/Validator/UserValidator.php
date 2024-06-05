e1f68f64284bc7bb8084bc6ccf8d0779f3082c69
Denis Pushkarev <igoo10on@gmail.com>
2024-06-06 00:36:54 +0200

    Add params to constructor fot forbidden words and domains

--- app/src/Validator/UserValidator.php
+++ app/src/Validator/UserValidator.php
@@ -6,17 +6,23 @@ use App\Exception\UserException;
 
 class UserValidator
 {
-    private static array $forbiddenWords = ['admin', 'nobody', 'test'];
-    private static array $unreliableDomains = ['kek.com', 'lol.me'];
+    private static array $forbiddenWords;
+    private static array $unreliableDomains;
+
+    public function __construct(array $forbiddenWords, array $unreliableDomains)
+    {
+        $this::$forbiddenWords = $forbiddenWords;
+        $this::$unreliableDomains = $unreliableDomains;
+    }
 
     public static function validateName(string $name): void
     {
         if (!preg_match('/^[a-z0-9]{8,}$/', $name)) {
-            throw new UserException('Invalid name format.');
+            throw new UserException('Invalid name format');
         }
         foreach (self::$forbiddenWords as $word) {
             if (str_contains($name, $word) !== false) {
-                throw new UserException('Name contains forbidden word.');
+                throw new UserException('Name contains forbidden word');
             }
         }
     }
@@ -24,18 +30,18 @@ class UserValidator
     public static function validateEmail(string $email): void
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new UserException('Invalid email format.');
+            throw new UserException('Invalid email format');
         }
         $domain = substr(strrchr($email, "@"), 1);
         if (in_array($domain, self::$unreliableDomains)) {
-            throw new UserException('Email domain is unreliable.');
+            throw new UserException('Email domain is unreliable');
         }
     }
 
     public static function validateDeletedDate(\DateTime $created, ?\DateTime $deleted): void
     {
         if ($deleted !== null && $deleted < $created) {
-            throw new UserException('Deleted date should be bigger or equal than created date.');
+            throw new UserException('Deleted date should be bigger or equal than created date');
         }
     }
 }
